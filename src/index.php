3ee57f3e0e323e4335472e130bce890b8c40a87e
chenchao <1366294101@qq.com>
2022-11-26 09:39:41 +0800

    refactor(page): 样式布局1
    [skip ci]

--- src/index.php
+++ src/index.php
@@ -11,13 +11,73 @@ $extra = $asset_8x8['simple1'];
 
 $map = new Matrix(8, 8, $extra);
 
-echo '<div>';
-foreach ($map->getMap() as $row) {
-    echo '<div style="display: flex">';
-    foreach ($row as $point) {
-        echo '<div style="display: inline-flex;width: 50px;height: 30px;border: 1px solid black; justify-content: center; align-items: center;">('
-            . $point->x . ',' . $point->y . ')' . '</div>';
+echo <<<EOF
+<!DOCTYPE html>
+<html lang="zh">
+  <head>
+    <title>路径搜索</title>
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        padding: 50px 100px;
+        background-color: #ff0;
+      }
+      .row {
+        display: flex;
+      }
+      .point {
+        display: inline-flex;
+        width: 50px;
+        height: 30px;
+        border: 1px solid black;
+        justify-content: center;
+        align-items: center;
+      }
+      #control {
+        height: 100px;
+        display: flex;
+        padding: 20px 50px;
+        background-color: red;
+        align-items: center;
+      }
+      .btn {
+        height: 40px;
+        padding: 0 10px;
+        margin: 0 20px
+      }
+    </style>
+  </head>
+  <body>
+EOF;
+
+
+echo '<div id="container">';
+echo '<div id="map">';
+foreach ($map->getMap() as $x => $row) {
+    echo '<div class="row" id="row' . $x . '">';
+    foreach ($row as $y => $point) {
+        echo '<div class="point" id="point' . $x . $y . '">('
+            . $x . ',' . $y . ')' . '</div>';
     }
     echo '</div>';
 }
 echo '</div>';
+// 控制按钮
+echo <<<EOF
+<div id="control">
+  <button class="btn">开始</button>
+  <button class="btn">重新开始</button>
+  <button class="btn">下一步</button>
+  <button class="btn">持续运行</button>
+</div>
+EOF;
+
+echo '</div>';
+
+echo <<<EOF
+  </body>
+</html>
+EOF;
