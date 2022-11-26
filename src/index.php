070ea8581eb3e7cdf4bfc7508b697549f329751c
chenchao <1366294101@qq.com>
2022-11-26 18:20:56 +0800

    refactor(page): 样式布局2
    [skip ci]

--- src/index.php
+++ src/index.php
@@ -16,6 +16,7 @@ echo <<<EOF
     <title>路径搜索</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/btn.css">
+    <link rel="stylesheet" type="text/css" href="css/input.css">
   </head>
   <body>
 EOF;
@@ -35,15 +36,17 @@ echo '</div>';
 // 输入框
 echo <<<EOF
 <div id="points">
-  <div class="point">
-    <span>起始点</span>
-    <p><span>x:&nbsp;</span><input type="text" /></p>
-    <p><span>y:&nbsp;</span><input type="text" /></p>
-  </div>
-  <div>
-    <span>终止点</span>
-    <p><span>x:&nbsp;</span><input type="text" /></p>
-    <p><span>y:&nbsp;</span><input type="text" /></p>
+  <div class="points_select">
+    <div class="point">
+      <span>起始点</span>
+      <p><span>x:&nbsp;</span><input name="src_x" type="text" /></p>
+      <p><span>y:&nbsp;</span><input name="src_y" type="text" /></p>
+    </div>
+    <div class="point">
+      <span>终止点</span>
+      <p><span>x:&nbsp;</span><input name="dst_x" type="text" /></p>
+      <p><span>y:&nbsp;</span><input name="dst_y" type="text" /></p>
+    </div>
   </div>
 </div>
 EOF;
@@ -51,7 +54,7 @@ EOF;
 // 控制按钮
 echo <<<EOF
 <div id="control">
-  <button class="btn disable">开始</button>
+  <button class="btn primary">开始</button>
   <button class="btn success">重新开始</button>
   <button class="btn info">下一步</button>
   <button class="btn warning">持续运行</button>
