60623da7fb93650217955dcc71b69708f6b236b5
chenchao <1366294101@qq.com>
2022-11-26 17:08:15 +0800

    refactor(page): 样式布局2
    [skip ci]

--- src/index.php
+++ src/index.php
@@ -4,8 +4,6 @@ require_once "../vendor/autoload.php";
 
 use common\map\Matrix;
 
-require_once "../vendor/autoload.php";
-
 $asset_8x8 = json_decode(file_get_contents('../assets/8x8.json'), true);
 $extra = $asset_8x8['simple1'];
 
@@ -16,39 +14,8 @@ echo <<<EOF
 <html lang="zh">
   <head>
     <title>路径搜索</title>
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-      }
-      #map {
-        padding: 50px 100px;
-        background-color: #ff0;
-      }
-      .row {
-        display: flex;
-      }
-      .point {
-        display: inline-flex;
-        width: 50px;
-        height: 30px;
-        border: 1px solid black;
-        justify-content: center;
-        align-items: center;
-      }
-      #control {
-        height: 100px;
-        display: flex;
-        padding: 20px 50px;
-        background-color: red;
-        align-items: center;
-      }
-      .btn {
-        height: 40px;
-        padding: 0 10px;
-        margin: 0 20px
-      }
-    </style>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/btn.css">
   </head>
   <body>
 EOF;
@@ -59,25 +26,42 @@ echo '<div id="map">';
 foreach ($map->getMap() as $x => $row) {
     echo '<div class="row" id="row' . $x . '">';
     foreach ($row as $y => $point) {
-        echo '<div class="point" id="point' . $x . $y . '">('
+        echo '<div class="node" id="node' . $x . $y . '">('
             . $x . ',' . $y . ')' . '</div>';
     }
     echo '</div>';
 }
 echo '</div>';
+// 输入框
+echo <<<EOF
+<div id="points">
+  <div class="point">
+    <span>起始点</span>
+    <p><span>x:&nbsp;</span><input type="text" /></p>
+    <p><span>y:&nbsp;</span><input type="text" /></p>
+  </div>
+  <div>
+    <span>终止点</span>
+    <p><span>x:&nbsp;</span><input type="text" /></p>
+    <p><span>y:&nbsp;</span><input type="text" /></p>
+  </div>
+</div>
+EOF;
+
 // 控制按钮
 echo <<<EOF
 <div id="control">
-  <button class="btn">开始</button>
-  <button class="btn">重新开始</button>
-  <button class="btn">下一步</button>
-  <button class="btn">持续运行</button>
+  <button class="btn disable">开始</button>
+  <button class="btn success">重新开始</button>
+  <button class="btn info">下一步</button>
+  <button class="btn warning">持续运行</button>
 </div>
 EOF;
 
 echo '</div>';
 
 echo <<<EOF
+  <script type="application/javascript" src="js/index.js"></script>
   </body>
 </html>
 EOF;
